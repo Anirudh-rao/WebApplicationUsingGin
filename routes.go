7208aaa817648501799929c016a15a7051da1675
Anirudh Rao <anirudh131299@gmail.com>
2022-05-11 10:40:09 +0530

    Article Changes

--- routes.go
+++ routes.go
@@ -1,6 +1,13 @@
+// routes.go
+
 package main
 
-func intializeRoutes() {
-	//handle the Index Route
+func initializeRoutes() {
+
+	// Handle the index route
 	router.GET("/", showIndexPage)
+
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
+
 }
